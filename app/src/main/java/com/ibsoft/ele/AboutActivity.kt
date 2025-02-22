16375962432af5a3c56ab0c9e5896f9b64f77244
ioannisb <ioannis.bouhras@gmail,com>
2025-02-22 14:39:22 +0200

    Second

--- app/src/main/java/com/ibsoft/ele/AboutActivity.kt
+++ app/src/main/java/com/ibsoft/ele/AboutActivity.kt
@@ -1,8 +1,11 @@
 package com.ibsoft.ele
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.ibsoft.ele.databinding.ActivityAboutBinding
+import android.text.Html
+
 
 class AboutActivity : AppCompatActivity() {
 
@@ -12,6 +15,8 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val textViewLicense = findViewById<TextView>(R.id.textViewLicense)
+        textViewLicense.text = Html.fromHtml(getString(R.string.license_text), Html.FROM_HTML_MODE_LEGACY)
 
 
     }
